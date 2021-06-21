b780ad8d5d3950fc0d4d930b908aa23de4c6b73b
Inspirelife96 <inspirelife@hotmail.com>
2021-06-21 18:10:10 +0800

    Initial commit

---
+++ Example/INSParse/INSViewController.h
@@ -0,0 +1,13 @@
+//
+//  INSViewController.h
+//  INSParse
+//
+//  Created by inspirelife@hotmail.com on 06/21/2021.
+//  Copyright (c) 2021 inspirelife@hotmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface INSViewController : UIViewController
+
+@end
---
+++ Example/INSParse/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  INSParse
+//
+//  Created by inspirelife@hotmail.com on 06/21/2021.
+//  Copyright (c) 2021 inspirelife@hotmail.com. All rights reserved.
+//
+
+@import UIKit;
+#import "INSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([INSAppDelegate class]));
+    }
+}
