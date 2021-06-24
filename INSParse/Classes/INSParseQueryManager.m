99332619381b96d8addfef35d4da2ceaecdfa209
Inspirelife96 <inspirelife@hotmail.com>
2021-06-24 10:36:25 +0800

    Initialize

---
+++ INSParse/Classes/INSParseQueryManager.m
@@ -0,0 +1,14 @@
+//
+//  INSParseQueryManager.m
+//  INSParse
+//
+//  Created by XueFeng Chen on 2021/6/22.
+//
+
+#import "INSParseQueryManager.h"
+
+@implementation INSParseQueryManager
+
+@end
+
+
